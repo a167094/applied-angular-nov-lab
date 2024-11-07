c8ec320c3dc02c730c7859a9abdaa6ca41de03bf
Johnathan Webster <johnathan_t_webster@progressive.com>
2024-11-07 15:08:17 -0500

    Extract Child component

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,12 +1,28 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { map } from 'rxjs';
+import { BookListComponent } from './components/book-list.component';
 
 @Component({
   selector: 'app-books',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterOutlet, RouterLink],
-  template: ` <p>Books</p> `,
+  imports: [RouterOutlet, RouterLink, JsonPipe, BookListComponent],
+  template: ` <p>Books</p>
+    <app-book-list [books]="books()" />`,
   styles: ``,
 })
-export class BooksComponent {}
+export class BooksComponent {
+  private client = inject(HttpClient);
+
+  books = toSignal(
+    this.client
+      .get<{
+        data: { id: string; title: string; author: string; year: number }[];
+      }>('/api/books')
+      .pipe(map((res) => res.data)),
+  );
+}
